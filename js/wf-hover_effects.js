a82614cb3a5dc403d99601b350aa21791862caef
jsnyd153 <93287904+jsnyd153@users.noreply.github.com>
2023-03-03 15:27:18 -0500

    change to init functions and delay run
    trying to let items load to avoid incorrect measurements from videos loading

--- js/wf-hover_effects.js
+++ js/wf-hover_effects.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    init_Tilt1();
+    init_Tilt2();
+  }, 1000);
+});
+
+function init_Tilt1() {
   const elements = document.querySelectorAll('[tilt="1"]');
 
   elements.forEach(function (element) {
@@ -66,9 +73,9 @@ document.addEventListener("DOMContentLoaded", function () {
         "perspective(500px) scale(1.1) rotateX(0) rotateY(0)";
     });
   });
-}); /* Store the element in el  - tilt 1*/
+} /* Store the element in el  - tilt 1*/
 
-document.addEventListener("DOMContentLoaded", function () {
+function init_Tilt2() {
   const elements = document.querySelectorAll('[tilt="2"]');
 
   elements.forEach(function (element) {
@@ -152,74 +159,4 @@ document.addEventListener("DOMContentLoaded", function () {
     //     "perspective(100px) scale(1) rotateX(0) rotateY(0)";
     // });
   });
-}); /* Store the element in el  - tilt 2*/
-
-// class HoverButton {
-//   constructor(el) {
-//     this.el = el;
-//     this.hover = false;
-//     this.calculatePosition();
-//     this.attachEventsListener();
-//   }
-
-//   attachEventsListener() {
-//     window.addEventListener('mousemove', e => this.onMouseMove(e));
-//     window.addEventListener('resize', e => this.calculatePosition(e));
-//   }
-
-//   calculatePosition() {
-//     gsap.set(this.el, {
-//       x: 0,
-//       y: 0,
-//       scale: 1 });
-
-//     const box = this.el.getBoundingClientRect();
-//     this.x = box.left + box.width * 0.5;
-//     this.y = box.top + box.height * 0.5;
-//     this.width = box.width;
-//     this.height = box.height;
-//   }
-
-//   onMouseMove(e) {
-//     let hover = false;
-//     let hoverArea = this.hover ? 0.7 : 0.5;
-//     let x = e.clientX - this.x;
-//     let y = e.clientY - this.y;
-//     let distance = Math.sqrt(x * x + y * y);
-//     if (distance < this.width * hoverArea) {
-//       hover = true;
-//       if (!this.hover) {
-//         this.hover = true;
-//       }
-//       this.onHover(e.clientX, e.clientY);
-//     }
-
-//     if (!hover && this.hover) {
-//       this.onLeave();
-//       this.hover = false;
-//     }
-//   }
-
-//   onHover(x, y) {
-//     gsap.to(this.el, {
-//       x: (x - this.x) * 0.2,
-//       y: (y - this.y) * 0.2,
-//       scale: 1.15,
-//       ease: 'power2.out',
-//       duration: 0.4 });
-
-//     this.el.style.zIndex = 10;
-//   }
-//   onLeave() {
-//     gsap.to(this.el, {
-//       x: 0,
-//       y: 0,
-//       scale: 1,
-//       ease: 'elastic.out(1.2, 0.4)',
-//       duration: 0.7 });
-
-//     this.el.style.zIndex = 1;
-//   }}
-
-// const btn1 = document.querySelector('#attract1');
-// new HoverButton(btn1);
+} /* Store the element in el  - tilt 2*/
